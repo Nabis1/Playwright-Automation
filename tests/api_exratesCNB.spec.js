ca3b39ed98215cd34d44d6eea3a612e55b2d02bf
Radim Černil <r.cernil332@gmail.com>
2024-07-27 00:55:18 +0200

    POM training

---
+++ tests/api_exratesCNB.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+import exrates from "../test-data/exratesDailyCNB.json";
+
+test.describe("CNB API verification", () => {
+
+    test("Verify multiple records returned against stored static response", async ({ page }) => {
+        const request = page.context().request
+        const response = await request.get("https://api.cnb.cz/cnbapi/exrates/daily")
+
+        const responseBody = await response.json()
+        expect(response.status()).toBe(200)
+        expect(responseBody).toEqual(exrates)
+        //console.log(responseBody)
+    })
+})
